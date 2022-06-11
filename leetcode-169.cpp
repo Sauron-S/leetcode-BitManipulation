41c3c050a65b4805499158c04c67bf5d1bc227bf
Saurabh Srivastava <46665920+Sauron-S@users.noreply.github.com>
2022-06-11 15:08:11 +0530

    Update leetcode-169.cpp
    Small change

--- leetcode-169.cpp
+++ leetcode-169.cpp
@@ -1,4 +1,6 @@
 /*
+169. Majority Element
+
 Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
@@ -13,7 +15,7 @@ Input: nums = [2,2,1,1,1,2,2]
 Output: 2
 */
 
-/*Solution using Boyer Morre's Voting Problem*/
+/*Solution using Boyer Morre's Voting Algorithm*/
 
 #include <bits/stdc++>
 
