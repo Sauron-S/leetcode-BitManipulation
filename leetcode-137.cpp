b4746a6fa9c3ff06d5823b2192f2c0db66d61a16
Saurabh Srivastava <46665920+Sauron-S@users.noreply.github.com>
2022-06-11 15:02:25 +0530

    Create leetcode-137.cpp
    Solution leetcode-137

---
+++ leetcode-137.cpp
@@ -0,0 +1,32 @@
+/*
+137. Single Number II
+
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+
+Input: nums = [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+*/
+
+#include <bits/stdc++>
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int one = 0;
+        int two = 0;
+        for(const auto& i:nums){
+            one = (one^i)&(~two);
+            two = (two^i)&(~one);
+        }
+      
+        return one;
+    }
+};
